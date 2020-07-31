e35d45e774d4e65f92af04edb10c20f9d8ba6198
unknown <venkat@codinggeek.com>
2020-07-31 13:50:22 +0530

    literals

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
 }
-var combinedAges = combine(30, 26);
+var combinedAges = combine(30, 26, 'as-number');
 console.log(combinedAges);
-var combinedNames = combine('Venkat', 'Raj');
+var combinedStringAges = combine('30', '26', 'as-text');
+console.log(combinedStringAges);
+var combinedNames = combine('Venkat', 'Raj', 'as-text');
 console.log(combinedNames);
