29ff0295e6d4a66e045dfdb269c8302d45459664
unknown <venkat@codinggeek.com>
2020-07-31 13:33:22 +0530

    enum

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["INSTRUCTOR"] = 1] = "INSTRUCTOR";
+    Role[Role["STUDENT"] = 2] = "STUDENT"; // 2
+})(Role || (Role = {}));
+var Test2;
+(function (Test2) {
+    Test2[Test2["ADMIN"] = 5] = "ADMIN";
+    Test2[Test2["INSTRUCTOR"] = 6] = "INSTRUCTOR";
+    Test2[Test2["STUDENT"] = 7] = "STUDENT"; // 7
+})(Test2 || (Test2 = {}));
 var person = {
     // const person = {
     name: 'Venkat',
     age: 21,
     hobbies: ['Sports', 'Cooking'],
-    role: [1, 'admin'],
+    role: Role.ADMIN,
     sayHi: function () {
         return "Hello, " + this.name;
     }
@@ -16,6 +28,18 @@ favActivities = ['Sports', 'Cooking'];
 var favActivities1;
 favActivities1 = ['Sports', 1, true];
 console.log(person.sayHi());
+console.log(person.role);
+switch (person.role) {
+    case 0:
+        console.log('ADMIN');
+        break;
+    case 1:
+        console.log('INSTRUCTOR');
+        break;
+    case 2:
+        console.log('STUDENT');
+        break;
+}
 for (var _i = 0, favActivities_1 = favActivities; _i < favActivities_1.length; _i++) {
     var hobby = favActivities_1[_i];
     // inferred correct as string, so string methods as available
--- app.ts
+++ app.ts
@@ -1,15 +1,28 @@
+enum Role {
+  ADMIN,       // 0
+  INSTRUCTOR,  // 1
+  STUDENT      // 2
+}
+
+enum Test2 {
+  ADMIN = 5,
+  INSTRUCTOR, // 6
+  STUDENT // 7
+}
+
 const person: {
   name: string
   age: number
   hobbies: string[]
-  role: [number, string] // tuple
+  // role: [number, string] // tuple
+  role: Role
   sayHi: any
 } = {
 // const person = {
   name: 'Venkat',
   age: 21,
   hobbies: ['Sports', 'Cooking'],
-  role: [1, 'admin'],
+  role: Role.ADMIN,
   sayHi() {
     return `Hello, ${this.name}`
   }
@@ -26,6 +39,18 @@ let favActivities1: any[]
 favActivities1 = ['Sports', 1, true]
 
 console.log(person.sayHi())
+console.log(person.role)
+switch(person.role) {
+  case 0:
+    console.log('ADMIN')
+    break;
+  case 1:
+    console.log('INSTRUCTOR')
+    break;
+  case 2:
+    console.log('STUDENT')
+    break;
+}
 
 for(const hobby of favActivities) {
   // inferred correct as string, so string methods as available
