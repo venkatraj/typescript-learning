5a7a065f6c2b6f3b0e89084b617e5885e97c4c5c
unknown <venkat@codinggeek.com>
2020-07-31 13:01:48 +0530

    type infer

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('App is running...@');
+function add(n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
+}
+var number1 = 5;
+var number2 = 2.8;
+var showResult = true;
+var phrase = 'Result is ';
+var result = add(number1, number2, showResult, phrase);
+// console.log(result) 
--- app.ts
+++ app.ts
@@ -1,1 +1,17 @@
-console.log('App is running...@')
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  const result = n1 + n2
+  if (showResult) {
+    console.log(phrase + result)
+  } else {
+
+    return result
+  }
+}
+
+let number1 = 5;
+const number2 = 2.8;
+const showResult = true
+const phrase = 'Result is '
+ 
+const result = add(number1, number2, showResult, phrase)
+// console.log(result) 
