2f7fb4055ac41336dd363bee5491994710d77111
unknown <venkat@codinggeek.com>
2020-07-31 18:04:20 +0530

    unions and types

---
+++ unions-types.ts
@@ -0,0 +1,24 @@
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text'
+
+function combine(
+  input1: Combinable, // type
+  input2: number | string, // union
+  resultConversion: ConversionDescriptor) {
+  let result;
+  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+    result = +input1 + +input2
+  } else {
+    result = input1.toString() + input2.toString()
+  }
+  return result
+}
+
+const combinedAges = combine(30, 26, 'as-number')
+console.log(combinedAges)
+
+const combinedStringAges = combine('30', '26', 'as-text')
+console.log(combinedStringAges)
+
+const combinedNames = combine('Venkat', 'Raj', 'as-text')
+console.log(combinedNames)
